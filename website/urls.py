e68d4644aa1b341aaed768522694664983889b9e
Aurnob-SYC <aurnob1syc@gmail.com>
2025-11-11 12:43:54 +0600

    Smart suggestion

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('search-suggestions/', views.search_suggestions, name='search-suggestions'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from product_list_smart_suggestion.models import Product
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')  
+
+def search_suggestions(request):
+    query = request.GET.get('q', '').strip()
+    if query:
+        results = list(Product.objects.filter(name__icontains=query).values('name')[:5])  # limit 5
+    else:
+        results = []
+    return JsonResponse(results, safe=False)
